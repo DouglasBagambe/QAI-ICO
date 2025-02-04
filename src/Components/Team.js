a2454c9c854874b1db7b8e3f414f55278b1901e3
douglasbagambe <douglasbagambe4@gmail.com>
2025-02-04 23:14:14 +0300

    init

---
+++ src/Components/Team.js
@@ -0,0 +1,87 @@
+// import React from 'react'
+// import Carousel from "react-multi-carousel";
+// import "react-multi-carousel/lib/styles.css";
+
+// const responsive = {
+//     superLargeDesktop: {
+//         // the naming can be any, depends on you.
+//         breakpoint: { max: 4000, min: 3000 },
+//         items: 5
+//     },
+//     desktop: {
+//         breakpoint: { max: 3000, min: 1024 },
+//         items: 3
+//     },
+//     tablet: {
+//         breakpoint: { max: 1024, min: 464 },
+//         items: 2
+//     },
+//     mobile: {
+//         breakpoint: { max: 464, min: 0 },
+//         items: 1
+//     }
+// };
+
+// const Team = () => {
+//     return (
+//         <>
+//             <Carousel responsive={responsive}>
+//                 <div className='grid gap-12 items-center md:grid-cols-3'>
+//                     <div class="space-y-4 text-center">
+//                         <img class="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64"
+//                             src="https://tailus.io/sources/blocks/classic/preview/images/woman1.jpg" alt="woman" loading="lazy" width="640" height="805" />
+//                         <div>
+//                             <h4 class="text-2xl">Hentoni Doe</h4>
+//                             <span class="block text-sm text-gray-500">CEO-Founder</span>
+//                         </div>
+//                     </div>
+//                     <div class="space-y-4 text-center">
+//                         <img class="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64"
+//                             src="https://tailus.io/sources/blocks/classic/preview/images/woman1.jpg" alt="woman" loading="lazy" width="640" height="805" />
+//                         <div>
+//                             <h4 class="text-2xl">Hentoni Doe</h4>
+//                             <span class="block text-sm text-gray-500">CEO-Founder</span>
+//                         </div>
+//                     </div>
+//                     <div class="space-y-4 text-center">
+//                         <img class="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64"
+//                             src="https://tailus.io/sources/blocks/classic/preview/images/woman1.jpg" alt="woman" loading="lazy" width="640" height="805" />
+//                         <div>
+//                             <h4 class="text-2xl">Hentoni Doe</h4>
+//                             <span class="block text-sm text-gray-500">CEO-Founder</span>
+//                         </div>
+//                     </div>
+//                     <div class="space-y-4 text-center">
+//                         <img class="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64"
+//                             src="https://tailus.io/sources/blocks/classic/preview/images/woman1.jpg" alt="woman" loading="lazy" width="640" height="805" />
+//                         <div>
+//                             <h4 class="text-2xl">Hentoni Doe</h4>
+//                             <span class="block text-sm text-gray-500">CEO-Founder</span>
+//                         </div>
+//                     </div>
+//                     <div class="space-y-4 text-center">
+//                         <img class="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64"
+//                             src="https://tailus.io/sources/blocks/classic/preview/images/woman1.jpg" alt="woman" loading="lazy" width="640" height="805" />
+//                         <div>
+//                             <h4 class="text-2xl">Hentoni Doe</h4>
+//                             <span class="block text-sm text-gray-500">CEO-Founder</span>
+//                         </div>
+//                     </div>
+//                     <div class="space-y-4 text-center">
+//                         <img class="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64"
+//                             src="https://tailus.io/sources/blocks/classic/preview/images/woman1.jpg" alt="woman" loading="lazy" width="640" height="805" />
+//                         <div>
+//                             <h4 class="text-2xl">Hentoni Doe</h4>
+//                             <span class="block text-sm text-gray-500">CEO-Founder</span>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 {/* <div>Item 2</div>
+//                 <div>Item 3</div>
+//                 <div>Item 4</div> */}
+//             </Carousel>
+//         </>
+//     )
+// }
+
+// export default Team
